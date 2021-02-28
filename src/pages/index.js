5ade66240f83b3c1e72b68b9190cf39df16d02b1
btbunze <btbunze@live.unc.edu>
2021-02-28 15:07:34 -0500

    moved imports to level_0

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React, { PureComponent } from "react";
-import { GameEngine } from "react-game-engine";
 import {Link} from "react-router-dom"
-import {Box} from '../renderers'
-import {moveBox} from '../systems'
+
 
 //GameEngine takes a list of systems and an object of entities (described by renderers)
 const Home = (props) => {

1c353117b51f6b11e459d6561ee810640a90e28c
Nelson Lopez <43620036+NelsonL19@users.noreply.github.com>
2021-02-28 14:39:54 -0500

    Set up prototype level file
    Set up the prototype level file to move our box demo to

---
+++ src/pages/level_0.js
@@ -0,0 +1,14 @@
+import React, {Component, useEffect, useState} from 'react';
+
+
+export default class Level_0 extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            data: null
+        }
+    }
+    render(){
+        return (<div>Prototype here!</div>)
+    }
+}

1cd3fcff793f63112145c7e748c8e17d1b596454
Nelson Lopez <43620036+NelsonL19@users.noreply.github.com>
2021-03-09 21:31:19 -0500

    Update ui_collect.jsx

--- src/components/ui_collect.jsx
+++ src/components/ui_collect.jsx
@@ -1,10 +1,10 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Button } from 'antd';
 import '../styles/ui_overlay.css'
 import Level_End from './level_end.jsx'
 
 export default class UI_Collect extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             data: null,
@@ -15,39 +15,39 @@ export default class UI_Collect extends Component {
         }
     }
 
-    componentDidMount() {
+    componentDidMount () {
         this.bonusTimerID = setInterval(
             () => this.tickDownBonus(), 10
         )
     }
 
-    componentWillUnmount() {
+    componentWillUnmount () {
         clearInterval(this.bonusTimerID)
     }
 
 
-    tickDownBonus(){
+    tickDownBonus () {
         // only count down if the bonus > 0 AND the level is still being played.
-        if(this.state.bonusScore <= 0 || this.state.isLevelComplete) {return}
+        if (this.state.bonusScore <= 0 || this.state.isLevelComplete) { return }
 
         this.setState({
             bonusScore: this.state.bonusScore - 1
         })
     }
 
-    checkIfDone(){
+    checkIfDone () {
         // make sure the level is not already done!
-        if (this.state.isLevelComplete) {return}
+        if (this.state.isLevelComplete) { return }
     }
 
-    render(){
+    render () {
         return (
             <div className="UI_Parent">
                 <div className="flex-container">
                     <div className="top-row">
                         <div>
-                        <h3>Level</h3>
-                        <h1 className="level">{this.props.level.levelNum}</h1>
+                            <h3>Level</h3>
+                            <h1 className="level">{this.props.level.levelNum}</h1>
                         </div>
                         <div>
                             <h3>Time Bonus</h3>
@@ -57,17 +57,26 @@ export default class UI_Collect extends Component {
                     <div className="bottom-row">
                         <h3>Evidence Collected:</h3>
                         <div className="collection-count">
+                            <table>
+                                <tr>
+                                    <th>1</th>
+                                    <th>2</th>
+                                    <th>3</th>
+                                    <th>4</th>
+                                    <th>5</th>
+                                </tr>
+                                </table>
+                        </div>
                         </div>
                     </div>
-                </div>
-                
-                <>
-                {this.state.isLevelComplete &&
-                    <Level_End info={this.state}/>
-                }
-                </>
 
-            </div>
+                    <>
+                        {this.state.isLevelComplete &&
+                            <Level_End info={this.state} />
+                        }
+                    </>
+
+                </div>
         )
     }
 }

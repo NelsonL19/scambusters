7fd99085739782ab6be2bc70dabeb3ac67f01ea7
Nelson Lopez <43620036+NelsonL19@users.noreply.github.com>
2021-03-16 16:04:19 -0400

    Adding the Version Tags
    Changed the Title while also making sure it was redeployed on Firebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,11 @@ const Home = (props) => {
   return (
     <>
       <h1>Welcome to Scambusters®: The Video Game</h1>
+      <p>Release Version: v0.2-alpha</p>
       <Link to = "/level0">Level 0</Link>
+      <br/>
       <Link to = "/level1">Level 1</Link>
+      <br/>
       <Link to = "/level2">Level 2</Link>
     </>
   )
